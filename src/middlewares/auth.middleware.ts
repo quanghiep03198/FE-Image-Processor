bbe88ae3bd48b1cddf2feebece5da98b415e105a
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-10 00:21:01 +0700

    refactor: update auth middleware to use unified request function and improve redirect paths

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,15 +1,20 @@
 import type { IUser } from '@/apis/auth/types'
-import { axiosRequestWithAuth } from '@/lib/axios'
+import { request } from '@/lib/request'
 import { redirect } from '@tanstack/react-router'
 import { createMiddleware } from '@tanstack/react-start'
 import { getCookie } from '@tanstack/react-start/server'
 
+export const guestMiddleware = createMiddleware().server(async ({ next }) => {
+  if (getCookie('access_token')) throw redirect({ to: '/' })
+  return await next()
+})
+
 export const authMiddleware = createMiddleware({ type: 'request' }).server(async ({ next }) => {
   const accessToken = getCookie('access_token')
-  if (!accessToken) throw redirect({ to: '/login' })
+  if (!accessToken) throw redirect({ to: '/signin' })
 
-  const user = await axiosRequestWithAuth<IUser>({ url: '/auth/profile', method: 'GET' })
-  if (!user) throw redirect({ to: '/login' })
+  const user = await request<IUser>({ url: '/auth/profile', method: 'GET' })
+  if (!user) throw redirect({ to: '/signin' })
 
   return next({ context: { user, accessToken } })
 })
